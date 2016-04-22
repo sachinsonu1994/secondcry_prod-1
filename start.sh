ec2278b1e490f245bc00534abc77534afb0f0049
Ankur Jain <ankurj.iitd@gmail.com>
2016-04-22 19:31:44 +0000

    Added footer image in code

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 bundle exec rake RAILS_ENV=production assets:clean
+sudo service mysql restart
+sudo service memcached restart
 
 bundle exec rake RAILS_ENV=production ts:index
 bundle exec rake RAILS_ENV=production ts:start
 bundle exec rake RAILS_ENV=production assets:precompile
 
-sudo service mysql restart
 nohup bundle exec rake RAILS_ENV=production jobs:work > log/delayed-job-worker.log 2>&1 &
 sudo service nginx restart
 #nohup bundle exec rails server -e production -b 0.0.0.0 > log/web-server.log 2>&1 &
