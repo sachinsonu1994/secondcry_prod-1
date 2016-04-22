ca6976eb8979f9117b50f41a4c984ee62c9cf6ca
Ankur Jain <ankurj.iitd@gmail.com>
2016-04-22 16:04:40 +0000

    Fixed startup scripts

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ bundle exec rake RAILS_ENV=production ts:index
 bundle exec rake RAILS_ENV=production ts:start
 bundle exec rake assets:precompile
 
+sudo service mysql restart
 nohup bundle exec rake RAILS_ENV=production jobs:work > log/delayed-job-worker.log 2>&1 &
 sudo service nginx restart
 #nohup bundle exec rails server -e production -b 0.0.0.0 > log/web-server.log 2>&1 &
