0da589e9b4d68cad41cafc3fd24a5654e5d5de9f
Sachin Gupta <sachinsonu1994@gmail.com>
2017-03-23 16:36:45 +0530

    Cleanup

--- app/controllers/google_merchant_controller.rb
+++ app/controllers/google_merchant_controller.rb
@@ -1,8 +1,6 @@
 # rubocop:disable ClassLength
 class GoogleMerchantController < ApplicationController
   def index
-    @selected_tribe_navi_tab = "home"
-
     respond_to do |format|
       # Keep format.html at top, as order is important for HTTP_ACCEPT headers with '*/*'
       format.html do
@@ -102,17 +100,13 @@ class GoogleMerchantController < ApplicationController
                locals: { listings: listings,
                          title: title,
                          updated: updated,
-                         brand_hash: brand_hash,
-
-                         # deprecated
-                         direction_map: direction_map
+                         brand_hash: brand_hash
                        }
       end
     end
   end
 
   private
-
 
   def build_title(params)
     category = Category.find_by_id(params["category"])
@@ -128,205 +122,6 @@ class GoogleMerchantController < ApplicationController
       :optional_category => category_label,
       :community_name => @current_community.name_with_separator(I18n.locale),
       :listing_type => listing_type_label)
-  end
-
-
-  def custom_field_value_factory(listing_id, custom_field_id, answer_value)
-    question = CustomField.find(custom_field_id)
-
-    answer = question.with_type do |question_type|
-      case question_type
-      when :dropdown
-        option_id = answer_value.to_i
-        answer = DropdownFieldValue.new
-        answer.custom_field_option_selections = [CustomFieldOptionSelection.new(:custom_field_value => answer,
-                                                                                :custom_field_option_id => answer_value,
-                                                                                :listing_id => listing_id)]
-        answer
-      when :text
-        answer = TextFieldValue.new
-        answer.text_value = answer_value
-        answer
-      when :numeric
-        answer = NumericFieldValue.new
-        answer.numeric_value = ParamsService.parse_float(answer_value)
-        answer
-      when :checkbox
-        answer = CheckboxFieldValue.new
-        answer.custom_field_option_selections = answer_value.map { |value|
-          CustomFieldOptionSelection.new(:custom_field_value => answer, :custom_field_option_id => value, :listing_id => listing_id)
-        }
-        answer
-      when :date_field
-        answer = DateFieldValue.new
-        answer.date_value = Time.utc(answer_value["(1i)"].to_i,
-                                     answer_value["(2i)"].to_i,
-                                     answer_value["(3i)"].to_i)
-        answer
-      else
-        raise ArgumentError.new("Unimplemented custom field answer for question #{question_type}")
-      end
-    end
-
-    answer.question = question
-    answer.listing_id = listing_id
-    return answer
-  end
-
-  # Note! Requires that parent listing is already saved to DB. We
-  # don't use association to link to listing but directly connect to
-  # listing_id.
-  def upsert_field_values!(listing, custom_field_params)
-    custom_field_params ||= {}
-
-    # Delete all existing
-    custom_field_value_ids = listing.custom_field_values.map(&:id)
-    CustomFieldOptionSelection.where(custom_field_value_id: custom_field_value_ids).delete_all
-    CustomFieldValue.where(id: custom_field_value_ids).delete_all
-
-    field_values = custom_field_params.map do |custom_field_id, answer_value|
-      custom_field_value_factory(listing.id, custom_field_id, answer_value) unless is_answer_value_blank(answer_value)
-    end.compact
-
-    # Insert new custom fields in a single transaction
-    CustomFieldValue.transaction do
-      field_values.each(&:save!)
-    end
-  end
-
-  def is_answer_value_blank(value)
-    if value.is_a?(Hash)
-      value["(3i)"].blank? || value["(2i)"].blank? || value["(1i)"].blank?  # DateFieldValue check
-    else
-      value.blank?
-    end
-  end
-
-  def is_authorized_to_post
-    if @current_community.require_verification_to_post_listings?
-      unless @current_user.has_admin_rights_in?(@current_community) || @current_community_membership.can_post_listings?
-        redirect_to verification_required_listings_path
-      end
-    end
-  end
-
-  def numeric_field_ids(custom_fields)
-    custom_fields.map do |custom_field|
-      custom_field.with(:numeric) do
-        custom_field.id
-      end
-    end.compact
-  end
-
-  def normalize_price_params(listing_params)
-    currency = listing_params[:currency]
-    listing_params.inject({}) do |hash, (k, v)|
-      case k
-      when "price"
-        hash.merge(:price_cents =>  MoneyUtil.parse_str_to_subunits(v, currency))
-      when "shipping_price"
-        hash.merge(:shipping_price_cents =>  MoneyUtil.parse_str_to_subunits(v, currency))
-      when "shipping_price_additional"
-        hash.merge(:shipping_price_additional_cents =>  MoneyUtil.parse_str_to_subunits(v, currency))
-      else
-        hash.merge(k.to_sym => v)
-      end
-    end
-  end
-
-  def payment_setup_status(community:, user:, listing:, payment_type:, process:)
-    case [payment_type, process]
-    when matches([nil]),
-         matches([__, :none])
-      [true, ""]
-    when matches([:braintree])
-      can_post = !PaymentRegistrationGuard.new(community, user, listing).requires_registration_before_posting?
-      settings_link = payment_settings_path(community.payment_gateway.gateway_type, user)
-      error_msg = t("listings.new.you_need_to_fill_payout_details_before_accepting", :payment_settings_link => view_context.link_to(t("listings.new.payment_settings_link"), settings_link)).html_safe
-
-      [can_post, error_msg]
-    when matches([:paypal])
-      can_post = PaypalHelper.community_ready_for_payments?(community.id)
-      error_msg =
-        if user.has_admin_rights_in?(community)
-          t("listings.new.community_not_configured_for_payments_admin",
-            payment_settings_link: view_context.link_to(
-              t("listings.new.payment_settings_link"),
-              admin_paypal_preferences_path()))
-            .html_safe
-        else
-          t("listings.new.community_not_configured_for_payments",
-            contact_admin_link: view_context.link_to(
-              t("listings.new.contact_admin_link_text"),
-              new_user_feedback_path))
-            .html_safe
-        end
-      [can_post, error_msg]
-    else
-      [true, ""]
-    end
-  end
-
-  def delivery_config(require_shipping_address, pickup_enabled, shipping_price, shipping_price_additional, currency)
-    shipping = delivery_price_hash(:shipping, shipping_price, shipping_price_additional)
-    pickup = delivery_price_hash(:pickup, Money.new(0, currency), Money.new(0, currency))
-
-    case [require_shipping_address, pickup_enabled]
-    when matches([true, true])
-      [shipping, pickup]
-    when matches([true, false])
-      [shipping]
-    when matches([false, true])
-      [pickup]
-    else
-      []
-    end
-  end
-
-  def create_listing_params(params)
-    listing_params = params.except(:delivery_methods).merge(
-      require_shipping_address: Maybe(params[:delivery_methods]).map { |d| d.include?("shipping") }.or_else(false),
-      pickup_enabled: Maybe(params[:delivery_methods]).map { |d| d.include?("pickup") }.or_else(false),
-      price_cents: params[:price_cents],
-      shipping_price_cents: params[:shipping_price_cents],
-      shipping_price_additional_cents: params[:shipping_price_additional_cents],
-      currency: params[:currency]
-    )
-
-    add_location_params(listing_params, params)
-  end
-
-  def add_location_params(listing_params, params)
-    if params[:origin_loc_attributes].nil?
-      listing_params
-    else
-      location_params = params[:origin_loc_attributes].permit(
-        :address,
-        :google_address,
-        :latitude,
-        :longitude
-      ).merge(
-        location_type: :origin_loc
-      )
-
-      listing_params.merge(
-        origin_loc_attributes: location_params
-      )
-    end
-  end
-
-  def get_transaction_process(community_id:, transaction_process_id:)
-    opts = {
-      process_id: transaction_process_id,
-      community_id: community_id
-    }
-
-    TransactionService::API::Api.processes.get(opts)
-      .maybe[:process]
-      .or_else(nil)
-      .tap { |process|
-        raise ArgumentError.new("Cannot find transaction process: #{opts}") if process.nil?
-      }
   end
 
   def listings_api
@@ -370,14 +165,5 @@ class GoogleMerchantController < ApplicationController
     else
       raise ArgumentError.new(shape_res.error_msg) unless shape_res.success
     end
-  end
-
-  def delivery_price_hash(delivery_type, price, shipping_price_additional)
-      { name: delivery_type,
-        price: price,
-        shipping_price_additional: shipping_price_additional,
-        price_info: ListingViewUtils.shipping_info(delivery_type, price, shipping_price_additional),
-        default: true
-      }
   end
 end
