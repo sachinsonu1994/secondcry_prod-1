15f6b2f754548f4a6420c5d2429bdd9478b0baf9
Sachin Gupta <sachinsonu1994@gmail.com>
2017-05-13 20:46:33 +0530

    Removed manual code

--- app/helpers/shipyarri_shipment_helper.rb
+++ app/helpers/shipyarri_shipment_helper.rb
@@ -1,8 +1,4 @@
 module ShipyarriShipmentHelper
-  SHIPYARRI_USERNAME = "demoAVNBIZ"
-  SHIPYARRI_CLIENTID = "1413"
-  SHIPYARRI_PARENTID = "333"
-  SHIPYARRI_URL = "http://avnbiz.co.in/test/AVNBIZ/webservice/test_upload_consignment.php"
 
   def self.get_label_from_shipyarri(tx_id, schedule_pickup_date)
     buyer_address = ShippingAddress.where("transaction_id = #{tx_id} and address_type = 'buyer'").first
